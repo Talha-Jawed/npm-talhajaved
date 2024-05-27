e7268ceb92270acc5160edea2f11cd18e198bec5
Talha javed <39768820+Talha-Jawed@users.noreply.github.com>
2024-05-28 03:38:22 +0500

    business card

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-function helloNpm() {
-  return "hello NPM";
-}
-console.log(helloNpm());
-module.exports = helloNpm;
+const { white, blueBright } = require("colorette");
+
+const heading = `${white("Talha Javed")}`;
+const work = white(`Software Engineer`);
+
+const linkedIn = blueBright(
+  "https://www.linkedin.com/in/talha-javed-450491176/"
+);
+const twitter = blueBright("https://twitter.com/talha_javed97");
+const github = blueBright("https://github.com/Talha-Jawed");
+const devTo = blueBright("https://dev.to/talhajawed");
+const website = blueBright("https://talha-jawed.github.io/portfolio/");
+const npx = white("npx talhajaved");
+
+const rows = {
+  LinkedIn: linkedIn,
+  Github: github,
+  DevTo: devTo,
+  Website: website,
+  Twitter: twitter,
+  Card: npx,
+};
+
+let output = `
+${heading}
+${work}
+  `;
+
+// `maxKeyLetters` helps in formatting the information nicely
+const maxKeyLetters = Math.max(...Object.keys(rows).map((key) => key.length));
+const secondaryContent = Object.keys(rows)
+  .map((key) => {
+    return `${key.padEnd(maxKeyLetters)} : ${rows[key]}`;
+  })
+  .join("\n");
+
+output += `
+${secondaryContent}
+  `;
+
+console.log(output);
